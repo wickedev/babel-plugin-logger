9f235809c61d194b1df8a3c86464f5e19950b228
wickedev <orange881217@gmail.com>
2019-11-19 23:36:03 +0900

    remove lib directory

--- lib/visitor.d.ts
+++
@@ -1,8 +0,0 @@
-import { NodePath } from '@babel/traverse';
-import { ArrowFunctionExpression, ClassMethod, FunctionDeclaration } from '@babel/types';
-import { Options } from '~/options';
-export declare function visitorFactory(options: Options): {
-    FunctionDeclaration(path: NodePath<FunctionDeclaration>): void;
-    ArrowFunctionExpression(path: NodePath<ArrowFunctionExpression>): void;
-    ClassMethod(path: NodePath<ClassMethod>, state: any): void;
-};
