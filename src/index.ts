1a54d99c876942418ee789581b08c203aa6a2968
wickedev <orange881217@gmail.com>
2019-12-01 16:34:04 +0900

    export from format to index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { defaultOptions, Options } from '~/options'
 import { visitorFactory } from '~/visitor'
 const packageJson = require('../package.json')
 
+export * from '~/format'
+
 // noinspection JSUnusedLocalSymbols
 export default declare((
     { types: t }: any /* api */,
